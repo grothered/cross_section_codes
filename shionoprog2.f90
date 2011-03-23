e339c8b39da8f39e5e3185bee9441aeb04ba1fc2
Gareth <gareth@gareth-laptop.(none)>
2011-03-23 23:22:31 +1100

    Experimentation with variable timestep

--- shionoprog2.f90
+++ shionoprog2.f90
@@ -512,7 +512,8 @@ DO Q_loop= 1, no_discharges!15
                 !    DT1 = min(DT,0.003_dp/max(maxval(abs(bed(l:u) - bedlast(l:u))), 0.0000001_dp)) 
                 !ELSEIF(.TRUE.) THEN
                     !tmp = max(maxval(abs(wset*C/rhos- Qe))/1.0_dp, maxval(abs(bed - bedlast)/DT1))
-                    tmp = min(maxval(abs(wset*C/rhos- Qe))*1.00_dp, maxval(abs(bed - bedlast)))
+                    tmp = min(maxval(abs(wset*C/rhos- Qe))*1.00_dp, &
+                              maxval(abs(bed(l+1:u-1) - bedlast(l+1:u-1))) ) 
                     !tmp = max(maxval(abs(wset*C/rhos- Qe)), maxval(abs(bed - bedlast)))
                     !tmp = max(maxval(abs(recrd(l-1:u)))*0.1_dp, maxval(abs(bed - bedlast)))
                     !tmp = max(maxval(abs(bed - bedlast)/DT1), 0.00001_dp)
