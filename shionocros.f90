edc4b5b009c7f7ea1ecfdab7887d3befd2659e4e
Gareth <gareth@gareth-laptop.(none)>
2011-03-29 21:36:39 +1100

    Further slight manning change, as I seem to be getting more convergent results.

--- shionocros.f90
+++ shionocros.f90
@@ -3974,7 +3974,7 @@ SUBROUTINE calc_friction(friction_type, grain_friction_type, rough_coef, water,&
             ! Manning style friction
             DO i= 1, a
                 !f(i)= rough_coef**2*g*8._dp/(max( water-bed(i),200._dp*d50)**(onethird))!
-                f(i)= rough_coef**2*g*8._dp/(max( water-bed(i),1.0e-05_dp)**(onethird))!
+                f(i)= rough_coef**2*g*8._dp/(max( water-bed(i),1.0e-08_dp)**(onethird))!
             END DO 
 
             ! Now spatially average the friction
