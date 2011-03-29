463ec328618065a79d0249582468aa3adfa268b3
Gareth <gareth@gareth-laptop.(none)>
2011-03-30 00:50:06 +1100

    Final commit before bed

--- more_tests/chi_2/input_data.f90
+++ more_tests/chi_2/input_data.f90
@@ -33,7 +33,7 @@ Width = 1500.0 !Width of computational domain (m)
 
 no_discharges = 1 !7
 Discharges = 2000.0  !2000.0  2000.0  2000.0  2000.0  2000.0  2000.0 
-susconcs =   8.0e-05 !4.0e-05 1.0e-05 5.0e-06 1.0e-06 !manning=.true. !Do we use a manning friction factor? If not, then we use a Darcy Weisbach friction factor, calculated as f=man_n**2*8.*9.8 -i.e., the equivalent manning friction value if the depth were 1 everywhere. Hence, the variable man_n is used to set either a manning or darcy friction. Same deal for vegetation drag
+susconcs =   1.0e-03 !4.0e-05 1.0e-05 5.0e-06 1.0e-06 !manning=.true. !Do we use a manning friction factor? If not, then we use a Darcy Weisbach friction factor, calculated as f=man_n**2*8.*9.8 -i.e., the equivalent manning friction value if the depth were 1 everywhere. Hence, the variable man_n is used to set either a manning or darcy friction. Same deal for vegetation drag
 friction_type = 'manning' !, 'darcy', 'vanrijn'
 rough_coef =0.032 !Mannings n for the basic bed
 grain_friction_type  = 'vanrijn' ! 'vanrijn', 'colebrook', 'onethird' -- model for bed shear acting on grains 
