ca07b42826d9ebb01b27b5b7fde2d98b3cc0de28
Gareth <gareth@gareth-laptop.(none)>
2011-04-17 18:57:05 +1000

    Minor change to cros.R

--- more_tests/farias/input_data.f90
+++ more_tests/farias/input_data.f90
@@ -25,7 +25,7 @@ writfreq = 2000 !24*5*5 ! The output is written every writfreq 'th timestep
 jmax= 1200000 !1382400 !24*5*2*45*4*4*4*4*5 ! The number of time steps
 t = 0.00 ! Starting time (s)
 dT = 1.0e-00 !Time step (s)
-variable_timestep=.FALSE. !Do we change the timestepping for high sediment concentrations? -- this is presently inconsistent with bed layers
+variable_timestep=.TRUE. !Do we change the timestepping for high sediment concentrations? -- this is presently inconsistent with bed layers
 
 waterM = 0.0 !Initial water elevation (m) and mean water elevation
 TR=0.0 !Tidal range. If it is set to zero then the discharge is constant, otherwise the continuity based method is used.
